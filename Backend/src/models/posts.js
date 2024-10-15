ecabd8921dda829a9a6854fc615b9cd70eecb436
Hoa-dvph <150907011+Hoa-dvph@users.noreply.github.com>
2024-10-15 15:31:59 +0700

    Fix model posts

--- Backend/src/models/posts.js
+++ Backend/src/models/posts.js
@@ -1,84 +1,52 @@
 import mongoose from 'mongoose';
 
-// Định nghĩa schema cho bình luận
-const commentSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    avatar: {
-        type: String, 
-        trim: true,
-    },
-    message: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-});
-
 // Định nghĩa schema cho bài viết
 const postSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
         trim: true,
+        required: true,
     },
     content: {
         type: String,
         required: true,
         trim: true,
+        required: true,
     },
     author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true,
-        },
-        name: {
-            type: String,
-            required: true,
-        }
-    },
-    avatar: {
-        type: String, 
-        trim: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
     },
     tags: [{
         type: String,
         trim: true,
     }],
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    updatedAt: {
-        type: Date,
-    },
     status: {
         type: String,
-        enum: ['draft', 'published', 'hidden'], 
+        enum: ['draft', 'published', 'hidden'],
         default: 'draft',
     },
-    views: {
+    images: [{
+        type: String,
+        trim: true,
+    }],
+    like_count: {
         type: Number,
         default: 0,
     },
-    image: {
-        type: String,  
-        trim: true,
-    },
-    comments: [commentSchema], 
-    topic: {
+    like: {
+        type: Array,
+        default: [],
+    },topic: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Topic',
         required: true,
     },
+}, {
+    timestamps: true,
+    versionKey: false,
 });
 
 // Tạo model từ schema
